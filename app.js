8a779876df670977b91f1fe8a1f4647e75f23118
winh321 <whmoney07@gmail.com>
2023-10-04 12:39:31 +0630

    file view

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+var fss = require('fs');
 var url = require('url');
+var path = require("path");
+imag_data = '';
+
+
 
 http.createServer(function (req, res) {
     
@@ -14,25 +19,46 @@ http.createServer(function (req, res) {
       res.write( "User Password : " + fields.password[0] + "<br>");
       res.write('File uploaded <br>');
       console.log("Successful!");
-      res.write(__dirname+ '\\upload\\'+files.filetoupload[0].originalFilename);
+      function added(xx){
+        res.write(xx);
+      }
+  
+      var newpath = __dirname+ '\\image\\'+files.filetoupload[0].originalFilename
+      var uploaded_img_path = 'image/'+files.filetoupload[0].originalFilename;
+      res.write(`<a href="${uploaded_img_path}"> ${'image/'+files.filetoupload[0].originalFilename} </a> `)
 
-      var newpath = __dirname+ '\\upload\\'+files.filetoupload[0].originalFilename
       var oldpath = files.filetoupload[0].filepath
-    
+
       fs.rename(oldpath, newpath, function (err) {
-    
         if (err) throw err;
-        res.write('File uploaded and moved!');
+        res.write('<br> <br> File uploaded and moved! <br> <br>');
       });
 
-      res.end();
+                // read image file
+  fss.readFile(newpath, (err, data)=>{
+    // error handle
+    if(err) {
+      console.log("First Error");
+        throw err;
+    } 
+    // get image file extension name
+    const extensionName = path.extname(newpath);
+    
+    // convert image file to base64-encoded string
+    const base64Image = Buffer.from(data, 'binary').toString('base64');
+    
+    // combine all strings
+    const base64ImageStr = `data:image/${extensionName.split('.').pop()};base64,${base64Image}`;
+    console.log("image reached")
+    imag_data = base64ImageStr;
+    res.write(`<img src="${imag_data}" width="200px" >`)
+    res.end();
+})
     });
 
 
-  }else if(req.url == '/image'){
-    res.write("Hi")
-    res.end()
-  } else {
+  }else if(req.url == '/'){
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="upload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
@@ -40,6 +66,50 @@ http.createServer(function (req, res) {
     res.write('<input type="text" value="123456789" name="password"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
-    return res.end();
+    res.end();
+
+  } else {
+
+    	// Parsing the URL
+	const request = url.parse(req.url, true);
+
+	// Extracting the path of file
+	const action = request.pathname;
+
+	// Path Refinements
+	const filePath = path.join(__dirname,
+		action).split("%20").join(" ");
+
+	// Checking if the path exists
+	fs.exists(filePath, function (exists) {
+
+		if (!exists) {
+			res.writeHead(404, {
+				"Content-Type": "text/plain"
+			});
+			res.end("404 Not Found Par");
+			return;
+		}
+
+		// Extracting file extension
+		const ext = path.extname(action);
+
+		// Setting default Content-Type
+		const contentType = "text/plain";
+
+		// Setting the headers
+		res.writeHead(200, {
+			"Content-Type": contentType
+		});
+
+		// Reading the file
+		fs.readFile(filePath,
+			function (err, content) {
+				// Serving the image
+				res.end(content);
+			});
+	});
   }
+      
+	
 }).listen(8080);
